dc19fddb97c63eb2614521ab73f3935f8df8ac3c
lisciu24 <47155387+lisciu24@users.noreply.github.com>
2019-11-15 20:48:07 +0100

    moveToMouse

---
+++ Grinch/Assets/Scripts/MoveToMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToMouse : MonoBehaviour
+{
+    public bool move = false;
+    public float velocity = 0F;
+    private Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = transform.position.z;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, target, velocity * Time.deltaTime);
+
+        move = (transform.position != target ? true : false);
+    }
+}
