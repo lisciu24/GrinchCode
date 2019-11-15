5208502cafdb3828f6adfacf9225a08b87c598bb
lisciu24 <47155387+lisciu24@users.noreply.github.com>
2019-11-15 22:44:53 +0100

    bug

--- Grinch/Assets/Scripts/TurretSpawn.cs
+++ Grinch/Assets/Scripts/TurretSpawn.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class TurretSpawn : MonoBehaviour
 {
-
-    public GameObject turretSelect;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +18,12 @@ public class TurretSpawn : MonoBehaviour
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out hit, 100.0F))
+            if (Physics.Raycast(ray, out hit, 1000.0F))
             {
                 if(hit.transform != null)
                 {
-                    Instantiate(turretSelect, new Vector3(0,0,0), Quaternion.identity);
+                    print("twoja stara gitara");
+                    Destroy(this.gameObject);
                 }
             }
         }
