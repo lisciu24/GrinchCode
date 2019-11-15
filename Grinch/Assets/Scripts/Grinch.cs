12fc01df66a857fc439d9b55cc5f3c5e91463297
lisciu24 <47155387+lisciu24@users.noreply.github.com>
2019-11-15 21:41:21 +0100

    turretSpawn

--- Grinch/Assets/Scripts/Grinch.cs
+++ Grinch/Assets/Scripts/Grinch.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveToMouse : MonoBehaviour
+public class Grinch : MonoBehaviour
 {
+    //living
+    public short healthPoints = 0;
+
+    //resources
+    public short redPresents = 0;
+    public short bluePresents = 0;
+    public short greenPresents = 0;
+
+    //moving
     public bool move = false;
     public float velocity = 0F;
     private Vector3 target;
+
+    //fighting
+    public float attackDamage = 0F;
+    public float attackSpeed = 0F;
+    public float range = 0F;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +31,18 @@ public class MoveToMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MoveToMouse();
+    }
+
+    void MoveToMouse()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             target.z = transform.position.z;
         }
-        transform.position = Vector3.MoveTowards(transform.position, target, velocity * Time.deltaTime);
 
+        transform.position = Vector3.MoveTowards(transform.position, target, velocity * Time.deltaTime);
         move = (transform.position != target ? true : false);
     }
 }
