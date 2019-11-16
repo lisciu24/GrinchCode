7a7399f9cbc27351737ebaa514382a81aa65a342
Wisnia12 <szymon270602@gmail.com>
2019-11-16 07:17:06 +0100

    cmd

---
+++ Grinch/Assets/PlayerDmg.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDmg : MonoBehaviour
+{
+
+    GameObject elf;
+    public float TimeBetweenAttacks = 0.5f;
+    public int attackDmg = 10;
+    EnemyHealth enemyHealth;
+    bool elfInRange;
+    float timer;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == elf)
+        {
+            elfInRange = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == elf)
+        {
+            elfInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= TimeBetweenAttacks && elfInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        timer = 0f;
+        if (enemyHealth.currentHealth > 0)
+        {
+            enemyHealth.TakeDamage(attackDmg);
+        }
+    }
+}
